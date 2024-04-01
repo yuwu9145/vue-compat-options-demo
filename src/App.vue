bcb46307ec407b35de61c91351627e374783b2c4
Yuchao Wu <yuchao.sydneyuni@gmail.com>
2024-04-01 12:10:02 +1100

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  created() {
+    this.$options.test = 'test'
+    console.log(this.$options)
+  }
+}
+</script>
+
+<template>
+  <h1>Open Console log and test property is available on $options</h1>
+</template>
+
